82cb2f00062100f2173ec8dc7e0b47f58c9b5edb
Michael Sasse <michaellavonsasse@gmail.com>
2021-11-11 02:30:14 -0600

    Remove and ignore /bin/

--- build.sh
+++ build.sh
@@ -8,11 +8,10 @@ if [ `expr length "$2"` == 0 ]; then
     exit 0
 fi
 
-
+mkdir -p bin
 cd "$1"
 asm="$2.asm"
 bin="../bin/$2.bin"
 echo "Make $asm"
-mkdir -p bin
 nasm -f bin "$asm" -o "$bin"
 qemu-system-x86_64 "$bin"
